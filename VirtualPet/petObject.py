d292ce7948490e9907a93f18d19a877689ebef61
Adam Clemons <adam@adamclmns.com>
2014-12-24 11:00:55 -0600

    Added PetDirtiness and pet.clean()
    The pet will now get dirty when it plays or feeds and the pet can be
    cleaned.

--- VirtualPet/petObject.py
+++ VirtualPet/petObject.py
@@ -4,27 +4,23 @@ class petClass:
 #Defining Initiation method - Sets up defaults when new object is created
     def __init__(self, name):
         self.name=name
-        self.size=1
-        self.color="blue"
         self.hunger=5
         self.happiness=5
+        self.dirtiness=0
 
 
 #Getters for Properties
     def getName(self):
         return self.name
 
-    def getSize(self):
-        return self.size
-
-    def getColor(self):
-        return self.color
-
     def getHunger(self):
         return self.hunger
 
     def getHappiness(self):
         return self.happiness
+
+    def getDirtiness(self):
+        return self.dirtiness
 
 #Functions that change or update property values
     def setName(self,name):
@@ -39,20 +35,17 @@ class petClass:
         '''Feed your pet to increase it's 'hunger' '''
         if(self.hunger < 10):
             self.hunger +=1
+            self.dirtiness +=1
             return
         else:
             return
-
-    def grow(self):
-        ''' your pet can get bigger'''
-        self.size +=1
-        return
 
     def play(self):
         '''play with your pet, they will use up some of their hunger, so be sure to feed them'''
         if(self.hunger > 0):
             self.hunger -= 1
             self.happiness += 1
+            self.dirtiness +=1
             return
         else:
             return
@@ -64,4 +57,9 @@ class petClass:
             return
         else:
             return
+
+    def clean(self):
+        self.dirtiness=0
+        self.happiness +=1
+
 
--- VirtualPet/uiObject.py
+++ VirtualPet/uiObject.py
@@ -36,32 +36,35 @@ class uiClass:
         #Create the variables for the labels.
         self.petHunger = StringVar()
         self.petHappiness = StringVar()
-        self.petSize=StringVar()
-        #self.petCleanliness=StringVar()
+        self.petDirtiness=StringVar()
 
         #Set variables for the UI
         self.petHunger.set(self.pet.getHunger())
         self.petHappiness.set(self.pet.getHappiness())
-        self.petSize.set(self.pet.getSize())
+        self.petDirtiness.set(self.pet.getDirtiness())
 
-        #Adding the Labels
+        #Adding the Labels for the left column
         Label(self.mainframe, text="Hunger").grid(column=1, row=1, sticky=(W))
         Label(self.mainframe, text="Happiness").grid(column = 1, row=2, sticky=(W))
-        Label(self.mainframe, text="Size").grid(column = 1, row=3, sticky=(W))
+        Label(self.mainframe, text="Dirtiness").grid(column=1, row=3, sticky=(W))
+
+        #Interactive Labels with variables that will change for the middle column
         Label(self.mainframe, textvariable=self.petHunger).grid(column=2, row=1, sticky=(W,E))
         Label(self.mainframe, textvariable=self.petHappiness).grid(column=2, row = 2, sticky=(W,E))
-        Label(self.mainframe, textvariable=self.petSize).grid(column=2, row=3, sticky=(W,E))
+        Label(self.mainframe, textvariable=self.petDirtiness).grid(column=2, row=3, sticky=(W,E))
 
-        #Adding buttons
+        #Adding buttons for the far right column
         Button(self.mainframe, text="Feed", command=self.feed).grid(column=4, row=1, sticky=(E))
         Button(self.mainframe, text="Play", command=self.play).grid(column=4, row=2, sticky=(E))
+        Button(self.mainframe, text="Clean", command=self.clean).grid(column=4, row=3, sticky=(E))
 
-        #adding image varriables to put into UI
+        #adding image variables to put into UI
         self.happyPet = ImageTk.PhotoImage(file=".\\images\\happyPet.png")
         self.sadPet = ImageTk.PhotoImage(file=".\\images\\sadPet.png")
         self.playPet = ImageTk.PhotoImage(file=".\\images\\playPet.png")
         self.feedPet = ImageTk.PhotoImage(file=".\\images\\feedPet.png")
 
+        #Setting a default starting image
         self.icon = Label(image=self.happyPet)
         self.icon.grid(column=0, row=4, columnspan=5, rowspan=5)
         self.icon.image = self.happyPet
@@ -75,18 +78,26 @@ class uiClass:
 
     def feed(self):
         self.pet.feed()
-        self.petHunger.set(self.pet.getHunger())
+        self.refreshData()
         self.changeImage(self.feedPet)
         return
 
     def play(self):
         self.pet.play()
-        self.petHappiness.set(self.pet.getHappiness())
-        self.petHunger.set(self.pet.getHunger())
+        self.refreshData()
         self.changeImage(self.playPet)
         return
+
+    def clean(self):
+        self.pet.clean()
+        self.refreshData()
 
     def timedUpdate(self):
         #Update function for cleanliness, hunger, happiness, and boredom will
 
         return
+
+    def refreshData(self):
+        self.petDirtiness.set(self.pet.getDirtiness())
+        self.petHappiness.set(self.pet.getHappiness())
+        self.petHunger.set(self.pet.getHunger())
