22bcdbaab10427520d70edea05c572bc49d2fbb2
Adam Clemons <adam@adamclmns.com>
2015-04-04 17:34:25 -0500

    Added iamge to the frame, got the image sizes right, and fixed the layout of the frame to look better

--- VirtualPet/PetView.py
+++ VirtualPet/PetView.py
@@ -5,6 +5,8 @@ This is a TKinter interface for the VirtualPet object class. This UI will intera
 
 __author__ = 'Adam Clemons'
 import tkinter as tk
+import os
+
 
 
 class PetView(tk.Frame):
@@ -17,10 +19,7 @@ class PetView(tk.Frame):
         self.mainframe.grid(column=0, row=0)
         self.mainframe.columnconfigure(0, weight=1)
         self.mainframe.rowconfigure(0, weight=1)
-        self.RestBadger = tk.PhotoImage(file="Images/RestBadger.png")
-        self.PlayBadger = tk.PhotoImage(file="Images/PlayBadger.png")
-        self.Eatbadger = tk.PhotoImage(file="Images/EatBadger.png")
-        self.CleanBadger = tk.PhotoImage(file="Images/CleanBadger.png")
+
         # This is for talking to the Controller
         self.ControllerCallback = CallbackController
 
@@ -28,6 +27,12 @@ class PetView(tk.Frame):
         self.petHunger = tk.StringVar()
         self.petHappiness = tk.StringVar()
         self.petCleanliness = tk.StringVar()
+        #Define them here in __init__ so we can make the LoadElements() function later
+        self.petPicture = None
+        self.RestBadger = None
+        self.PlayBadger = None
+        self.CleanBadger = None
+        self.Eatbadger = None
 
         #Set starting values of the labels
         self.petHappiness.set(str(14))
@@ -39,26 +44,35 @@ class PetView(tk.Frame):
     # TODO: Make View setup a new function loadElements()
     #
         # Adding the Labels
-        tk.Label(self.mainframe, text="Hunger ").grid(column=2, row=4)
-        tk.Label(self.mainframe, text="Happiness ").grid(column=2, row=5)
-        tk.Label(self.mainframe, text="Size ").grid(column=2, row=6)
-        tk.Label(self.mainframe, textvariable=self.petHunger).grid(column=3, row=4)
-        tk.Label(self.mainframe, textvariable=self.petHappiness).grid(column=3, row=5)
-        tk.Label(self.mainframe, textvariable=self.petCleanliness).grid(column=3, row=6)
+        tk.Label(self.mainframe, text="Hunger ").grid(column=2, row=5)
+        tk.Label(self.mainframe, text="Happiness ").grid(column=2, row=6)
+        tk.Label(self.mainframe, text="Size ").grid(column=2, row=7)
+        tk.Label(self.mainframe, textvariable=self.petHunger).grid(column=3, row=5)
+        tk.Label(self.mainframe, textvariable=self.petHappiness).grid(column=3, row=6)
+        tk.Label(self.mainframe, textvariable=self.petCleanliness).grid(column=3, row=7)
         # Adding a picture Label
+        # TODO: Demonstrate the Python Console
+        # print(os.getcwd())
+        path=os.getcwd()
+        self.RestBadger = tk.PhotoImage(file=path+"\Images\RestingBadger-small.png")
+        self.PlayBadger = tk.PhotoImage(file=path+"\Images\PlayBadger-small.png")
+        self.EatBadger = tk.PhotoImage(file=path+"\Images\EatBadger-small.png")
+        self.CleanBadger = tk.PhotoImage(file=path+"\Images\CleanBadger-small.png")
+        self.petPicture = tk.Label(self.mainframe, image=self.RestBadger).grid(column=1, row=1, columnspan=6, rowspan=4)
+        # Adding a placeholder label to put some empty space at the bottom of the window
+        tk.Label(self.mainframe, text=' ').grid(column=3, row=8)
 
-        # Adding buttons
-        # TODO: Add Actions to the Buttons
-        # TODO: Use Lambdas instead of wrapper functions
+
+         # Adding buttons
+        # TODO: Demonstrate how to use Lambdas instead of wrapper functions
         # tk.Button(self.mainframe, text="Feed", name="feed").grid(column=5, row=4)
         # tk.Button(self.mainframe, text="Play", name="play").grid(column=5, row=5)
         # tk.Button(self.mainframe, text="Clean", name="clean").grid(column=5, row=6)
 
-        tk.Button(self.mainframe, text="Feed", name="feed", command=self.ControllerCallback.feedPressed).grid(column=5, row=4)
-        tk.Button(self.mainframe, text="Play", name="play", command=self.ControllerCallback.playPressed).grid(column=5, row=5)
-        tk.Button(self.mainframe, text="Clean", name="clean", command=self.ControllerCallback.cleanPressed).grid(column=5, row=6)
+        tk.Button(self.mainframe, text="Feed", name="feed", command=self.ControllerCallback.feedPressed).grid(column=5, row=5)
+        tk.Button(self.mainframe, text="Play", name="play", command=self.ControllerCallback.playPressed).grid(column=5, row=6)
+        tk.Button(self.mainframe, text="Clean", name="clean", command=self.ControllerCallback.cleanPressed).grid(column=5, row=7)
 
-    # TODO: Create Action Setting functions here for passing to the Controller
     def setHappiness(self, value):
         self.petHappiness.set(value)
 
@@ -68,6 +82,15 @@ class PetView(tk.Frame):
     def setCleanliness(self, value):
         self.petCleanliness.set(value)
 
+    # Create functions
+    def ResetImage(self):
+        self.petPicture.set(image=self.RestBadger)
 
+    def Feed(self):
+        self.petPicture.set(image=self.EatBadger)
 
+    def Clean(self):
+        self.petPicture.set(image=self.CleanBadger)
 
+    def Play(self):
+        self.petPicture.set(image=self.PlayBadger)
